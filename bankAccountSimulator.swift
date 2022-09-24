a7911415bcab2c788f8561610985cd03fd08ebb4
Tyler Weinacker <tweinacker57@gmail.com>
2022-09-24 11:28:37 -0400

    Add functionality by adding switch statement and printing the type of account

--- bankAccountSimulator.swift
+++ bankAccountSimulator.swift
@@ -9,4 +9,11 @@ repeat {
     print("Which option do you choose? (1, 2, or 3)")
     input = Int.random(in: 1...5)
     print("The selected option is \(input).")
-}
+    switch input {
+    case 1: accountType = "debit"
+    case 2: accountType = "credit"
+    case 3: accountType = "generic"
+    default: break
+    }
+}while accountType == ""
+    print("You have opened a \(accountType) account.")
