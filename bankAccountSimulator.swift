b24055cc90ac773fac6e8b4fbd9973b33751c81e
Tyler Weinacker <tweinacker57@gmail.com>
2022-09-24 11:02:32 -0400

    Initial commit with welcome statements and account type and user input variables

---
+++ bankAccountSimulator.swift
@@ -0,0 +1,8 @@
+//Create a bank account simulator app. Part 1 is creating the bank account
+print("Welcome to your virtual bank system. What kind of account would you like to make?")
+print("1. Debit account")
+print("2. Credit account")
+print("3. Bank account")
+var accountType = ""
+var input = 0
+
